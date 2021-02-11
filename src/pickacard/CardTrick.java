b2c49fc3cad2d69117dd511089b6bf21821130bd
Ek-noor <78036257+Ek-noor@users.noreply.github.com>
2021-02-11 03:37:49 -0500

    Made code to generate the random number

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -17,17 +17,24 @@ public class CardTrick {
 public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        Random ran = new Random();
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            
             //c.setValue(insert call to random number generator here)
+            c.setValue(ran.nextInt(13)+1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[ran.nextInt(4)]);
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        
         // and search magicHand here
+        
         //Then report the result here
+        
     }
     
 }
